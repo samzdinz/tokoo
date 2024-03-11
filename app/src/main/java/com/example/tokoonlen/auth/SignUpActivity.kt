5f692dec2c15f8313bc9fb26d8bf301b259ed46f
SamzDinz <samzdinz@gmail.com>
2024-03-11 14:25:03 +0700

    add sign in and sign up activity

---
+++ app/src/main/java/com/example/tokoonlen/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tokoonlen.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tokoonlen.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
